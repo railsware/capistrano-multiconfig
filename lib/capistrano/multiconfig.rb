69925c9dee1a45dc3c6601c248d64b205f50d882
Andriy Yanko <andriy.yanko@gmail.com>
2013-10-23 00:29:16 +0300

    issue 5: Fixed multiconfig dsl loading.

--- lib/capistrano/multiconfig.rb
+++ lib/capistrano/multiconfig.rb
@@ -1,47 +1,7 @@
+require 'capistrano/multiconfig/dsl'
+
 include Capistrano::DSL
-
-def stages_root
-  fetch(:stages_root, 'config/deploy')
-end
-
-# Build stages with nested configurations
-#
-# @example simple stages
-#
-#   config
-#   ├── deploy
-#   │   ├── production.rb
-#   │   └── staging.rb
-#   └── deploy.rb
-#
-# * cap production
-# * cap staging
-#
-# @example stages with nested configurations
-#
-#   config
-#   ├── deploy
-#   │   ├── soa
-#   │   │   ├── blog
-#   │   │   │   ├── production.rb
-#   │   │   │   └── staging.rb
-#   │   │   └── wiki
-#   │   │       └── qa.rb
-#   │   └── soa.rb
-#   └── deploy.rb
-#
-# * cap soa:blog:production
-# * cap soa:blog:staging
-# * cap soa:wiki:qa
-def stages
-  Dir["#{stages_root}/**/*.rb"].map { |file|
-    file.slice(stages_root.size + 1 .. -4).tr('/', ':')
-  }.tap { |paths|
-    paths.reject! { |path|
-      paths.any? { |another| another != path && another.start_with?(path) }
-    }
-  }.sort
-end
+include Capistrano::Multiconfig::DSL
 
 stages.each do |stage|
   Rake::Task.define_task(stage) do
---
+++ lib/capistrano/multiconfig/dsl.rb
@@ -0,0 +1,50 @@
+module Capistrano
+  module Multiconfig
+    module DSL
+      def stages_root
+        fetch(:stages_root, 'config/deploy')
+      end
+
+      # Build stages with nested configurations
+      #
+      # @example simple stages
+      #
+      #   config
+      #   ├── deploy
+      #   │   ├── production.rb
+      #   │   └── staging.rb
+      #   └── deploy.rb
+      #
+      # * cap production
+      # * cap staging
+      #
+      # @example stages with nested configurations
+      #
+      #   config
+      #   ├── deploy
+      #   │   ├── soa
+      #   │   │   ├── blog
+      #   │   │   │   ├── production.rb
+      #   │   │   │   └── staging.rb
+      #   │   │   └── wiki
+      #   │   │       └── qa.rb
+      #   │   └── soa.rb
+      #   └── deploy.rb
+      #
+      # * cap soa:blog:production
+      # * cap soa:blog:staging
+      # * cap soa:wiki:qa
+      def stages
+        Dir["#{stages_root}/**/*.rb"].map { |file|
+          file.slice(stages_root.size + 1 .. -4).tr('/', ':')
+        }.tap { |paths|
+          paths.reject! { |path|
+            paths.any? { |another| another != path && another.start_with?(path) }
+          }
+        }.sort
+      end
+    end
+  end
+end
+
+self.extend Capistrano::Multiconfig::DSL
